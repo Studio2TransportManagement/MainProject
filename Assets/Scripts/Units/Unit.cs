97d87b369414690c64d59cee92e80ec7f3127f13
Lachlan Brown <lachlan.brown8@live.com.au>
2015-03-06 13:08:51 +1100

    Added ProCore - Prototyping addon
    Made a second map for testing some camera/spawning stuff

---
+++ Assets/Scripts/Units/Unit.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Unit : MonoBehaviour {
+
+	public float fHealthMax;
+	public float fHealthCurrent;
+	public float fRange;
+	public float fFireRate;
+
+	public int iAmmo;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
