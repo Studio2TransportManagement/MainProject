36cdfcbf3506fad45f16cb88010d7f985f46260f
Koutacles <111412@student.sae.edu.au>
2015-03-06 14:51:59 +1100

    -Transport train test added -Installed LeanTween/ProCore

---
+++ Assets/Scripts/Units/Train.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Train : GameUnit {
+	
+
+	// Use this for initialization
+	void Start () {
+		bUnselectable = false;
+		//selectionManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<SelectionManager>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/Units/TrainTrack.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TrainTrack : MonoBehaviour {
+
+	public List<TrainStation> l_tsNodes;
+	public Train guTrain;
+
+	// Use this for initialization
+	void Start() {
+		TrainStation[] nodes = this.GetComponentsInChildren<TrainStation>();
+		l_tsNodes.AddRange(nodes);
+		guTrain = this.GetComponentInChildren<Train>();
+	}
+	
+	// Update is called once per frame
+	void Update() {
+		guTrain.GetComponent<SlideToLocation>().vTarget = l_tsNodes[0].transform.position;
+
+
+		//Temp
+		if (Input.GetKeyUp(KeyCode.T)) {
+			l_tsNodes.Reverse();
+		}
+	}
+}
