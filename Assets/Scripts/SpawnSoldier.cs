66d4a0aac8b74cb0b8b848559735563cd9938949
unknown <dhanna1@SYD-807-1544-W.syd.sae>
2015-03-11 11:58:45 +1100

    Added UI to game scene

---
+++ Assets/Scripts/SpawnSoldier.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+
+public class SpawnSoldier : MonoBehaviour {
+
+	public GameObject goProtoUnit;
+	//private Vector3 vSpawnPoint;
+	private string sName;
+
+	//Default choice for the enumerator. 
+	//private SOLDIER_TYPE troopChoice = SOLDIER_TYPE.GUNNER;
+
+	void Start() {
+//		this.GetComponent<Button>().onClick.AddListener(() => { 
+//			SpawnSoldier(troopChoice); 
+//		});
+	}
+
+	void Update() {
+		//
+	}
+
+    public void SpawnUnit(SOLDIER_TYPE sol, Vector3 spawnpoint, string name) {
+		if (sol == SOLDIER_TYPE.GUNNER) {
+			Debug.Log("GUNNER");
+			GameObject lastSpawned = (GameObject)Instantiate(goProtoUnit, spawnpoint, Quaternion.identity);
+			lastSpawned.GetComponent<GameUnit>().sUnitName = name;
+			//lastSpawned.GetComponent<SlideToLocation>().vTarget.x += Random.Range(-0.5f, 0.5f);
+		}
+
+		if (sol == SOLDIER_TYPE.HEAVY) {
+			Debug.Log("HEAVY");
+
+		}
+		if (sol == SOLDIER_TYPE.MEDIC) {
+			Debug.Log("MEDIC");
+		}
+		if (sol == SOLDIER_TYPE.MECHANIC) {
+			Debug.Log("MECHANIC");
+		}
+	}
+}
---
+++ Assets/Scripts/unitButton.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class unitButton : MonoBehaviour, IDropHandler {
+	
+	public GameObject goRecruitSystem;
+	public GameObject goButtonPressed;
+	public GameObject goCurrentlyTraining;
+	public List<string> l_sCurrentUnitsName = new List<string>();
+	public Text textCost;
+	public Color32 colTransparent;
+	public int iUnitsTraining = 0;
+	public int iPrice = 100;
+	public float fTrainingTimer = 60;
+
+	private SpawnSoldier unitSpawner;
+	//public GameObject goProtoUnit;
+	public SOLDIER_TYPE troopChoice = SOLDIER_TYPE.GUNNER;
+
+	void Start() {
+		unitSpawner = this.GetComponentInParent<SpawnSoldier> ();
+	}
+	
+	void Update ()
+	{
+		
+		textCost.text = "$" + iPrice + "";
+		
+		if (iUnitsTraining == 0)
+		{
+			goCurrentlyTraining.GetComponent<Text>().color = colTransparent;
+		}
+		else
+		{
+			goCurrentlyTraining.GetComponent<Text>().color = Color.white;
+			goCurrentlyTraining.GetComponent<Text>().text = "" + iUnitsTraining + "";
+		}
+		
+		if (iUnitsTraining >= 1)
+		{
+			goButtonPressed.GetComponent<Image> ().fillAmount -= Time.deltaTime / fTrainingTimer;
+			
+			if (goButtonPressed.GetComponent<Image> ().fillAmount == 0)
+			{
+				iUnitsTraining -= 1;
+				Debug.Log("Trained " + l_sCurrentUnitsName[0] +"");
+				//spawn new unit
+				//add name to newly spawned units script
+				unitSpawner.SpawnUnit(troopChoice, Vector3.zero, l_sCurrentUnitsName[0]);
+				l_sCurrentUnitsName.RemoveAt(0);
+			}
+			
+		}
+		
+		if (iUnitsTraining >= 1)
+		{
+			if (goButtonPressed.GetComponent<Image>().fillAmount == 0)
+			{
+				goButtonPressed.GetComponent<Image>().fillAmount = 1;
+			}	
+		}
+		
+	}
+	
+	#region IDropHandler implementation
+	
+	public void OnDrop (PointerEventData eventData)
+	{
+		if (Camera.main.GetComponent<stats>().cash >= iPrice && Camera.main.GetComponent<stats>().recruits >= 1)
+		{
+			l_sCurrentUnitsName.Add("" + goRecruitSystem.GetComponent<characterRandomiser>().characterName.GetComponent<Text>().text + "");
+			goRecruitSystem.GetComponent<characterRandomiser>().RandomiseAvatar();
+			if (iUnitsTraining == 0)
+			{
+				goButtonPressed.GetComponent<Image>().fillAmount = 1;
+				iUnitsTraining += 1;
+				Camera.main.GetComponent<stats>().cash -= iPrice;
+				Camera.main.GetComponent<stats>().recruits -= 1;
+			}
+			else if (iUnitsTraining >= 1)
+			{
+				iUnitsTraining += 1;
+				Camera.main.GetComponent<stats>().cash -= iPrice;
+				Camera.main.GetComponent<stats>().recruits -= 1;
+			}
+		}
+	}
+	
+	#endregion
+	
+	
+}
