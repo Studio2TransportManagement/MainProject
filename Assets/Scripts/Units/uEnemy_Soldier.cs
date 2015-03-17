6458e6a28bdd2f2382ebef51b30e8db74bfbc137
unknown <lbrown53@SYD-807-1545-W.syd.sae>
2015-03-18 09:29:13 +1100

    fasdfasdf

--- Assets/Scripts/Units/uEnemy_Soldier.cs
+++ Assets/Scripts/Units/uEnemy_Soldier.cs
@@ -17,11 +17,11 @@ public class uEnemy_Soldier : GameUnit {
 
 		agent.SetDestination(new Vector3(0,0,0));
 
-		//goTargetBase = GetClosestBase();
-
-		//agent.SetDestination(goTargetBase.transform.position);
-
-		//Debug.Log(StructureManager.bases[1].transform.position.ToString());
+//		goTargetBase = GetClosestBase();
+//
+//		agent.SetDestination(goTargetBase.transform.position);
+//
+//		Debug.Log(StructureManager.bases[1].transform.position.ToString());
 	}
 
 	// Update is called once per frame
@@ -33,22 +33,22 @@ public class uEnemy_Soldier : GameUnit {
 //		}
 	}
 
-	GameObject GetClosestBase()
-	{
-		GameObject returnObject = StructureManager.bases[0];
-				
-		float tempdistance = (float)10000.0;
-
-		for(int i = 0; i < StructureManager.bases.Length; i++)
-		{
-			if( Vector3.Distance(this.transform.position, StructureManager.bases[i].transform.position) < tempdistance)
-			{
-				tempdistance = Vector3.Distance(this.transform.position, StructureManager.bases[i].transform.position);
-				returnObject = StructureManager.bases[i];
-			}
-
-		}
-
-		return returnObject;
-	}
+//	GameObject GetClosestBase()
+//	{
+//		GameObject returnObject = StructureManager.bases[0];
+//				
+//		float tempdistance = (float)10000.0;
+//
+//		for(int i = 0; i < StructureManager.bases.Length; i++)
+//		{
+//			if( Vector3.Distance(this.transform.position, StructureManager.bases[i].transform.position) < tempdistance)
+//			{
+//				tempdistance = Vector3.Distance(this.transform.position, StructureManager.bases[i].transform.position);
+//				returnObject = StructureManager.bases[i];
+//			}
+//
+//		}
+//
+//		return returnObject;
+//	}
 }
