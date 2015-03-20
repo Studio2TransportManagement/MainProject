292b9b9137b0571284238b4b87847a826a5e7db2
Ethan Roberts <ethaninja@hotmail.com>
2015-03-20 15:32:59 +1100

    Fucked up tower script, still gotta merge it with finished tower script

---
+++ Assets/Scripts/Tower.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tower : MonoBehaviour {
+
+	private bool bIsEmpty;
+	public GameObject goStationedUnit;
+	public Camera cTowerCamera;
+	public Camera cMainCamera;
+	
+	//
+	void Start(){
+		cMainCamera.enabled = true;
+		cTowerCamera.enabled = false;
+		//bIsEmpty = true;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(bIsEmpty == false && Input.GetKeyDown(KeyCode.C))
+		{
+			SwitchCamera();
+		}
+
+	
+	}
+
+	public void SwitchCamera(){
+		cTowerCamera.enabled = !cTowerCamera.enabled;
+		cMainCamera.enabled = !cTowerCamera.enabled;
+		Screen.showCursor = !Screen.showCursor;
+	}
+
+	public void FillTower(){
+		bIsEmpty = false;
+	}
+
+	public void EmptyTower(){
+		bIsEmpty = true;
+		//cTowerCamera.camera.enabled = false;
+	}
+}
