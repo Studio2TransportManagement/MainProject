1817b9a3ed2ead7afc059d3b50655d458d642310
unknown <lbrown53@SYD-807-1545-W.syd.sae>
2015-04-14 12:11:46 +1000

    Deletion

--- Assets/Scripts/Structures/staticStructures.cs
+++
@@ -1,8 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-using System.Collections.Generic;
-
-public static class staticStructures {
-
-	public static List<BaseStructure> bases = new List<BaseStructure>();
-}
