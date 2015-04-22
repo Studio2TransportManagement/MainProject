c4a61a5ef2483a608789d7eda0c13f33c05a293d
unknown <dhanna1@SYD-805-490-W.syd.sae>
2015-04-22 16:13:48 +1000

    Minimap now changes color based on the percentage health of each base

--- Assets/Scripts/MiniMapBases.cs
+++ Assets/Scripts/MiniMapBases.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class MiniMapBases : MonoBehaviour {
@@ -7,11 +8,40 @@ public class MiniMapBases : MonoBehaviour {
 	public RectTransform canvasRectT;
 	public RectTransform BasesRectT;
 	public Transform tObjectToFollow;
+	public BaseGameStructure ThisBase;
+	public Image ICurrentBase;
+	public Sprite sBlue;
+	public Sprite sOrange;
+	public Sprite sRed;
+	public Sprite sBlinkingRed;
 	
 	void Update()
 	{
 		Vector2 screenPoint = RectTransformUtility.WorldToScreenPoint(MiniMapCamera, tObjectToFollow.position + new Vector3 (0f, 0f, 0f));
 		
 		BasesRectT.anchoredPosition = screenPoint - canvasRectT.sizeDelta / 2f;
+
+		if(ThisBase.fHealthCurrent <= ThisBase.fHealthMax &&
+		   ThisBase.fHealthCurrent > (3f/4f) * ThisBase.fHealthMax)
+		{
+			ICurrentBase.overrideSprite = sBlue;
+		}
+
+		if(ThisBase.fHealthCurrent <= (3f/4f) * ThisBase.fHealthMax &&
+		   ThisBase.fHealthCurrent > (1f/2f) * ThisBase.fHealthMax)
+		{
+			ICurrentBase.overrideSprite = sOrange;
+		}
+
+		if(ThisBase.fHealthCurrent <= (1f/2f) * ThisBase.fHealthMax &&
+		   ThisBase.fHealthCurrent > (1f/4f) * ThisBase.fHealthMax)
+		{
+			ICurrentBase.overrideSprite = sRed;
+		}
+
+		if(ThisBase.fHealthCurrent <= (1f/4f) * ThisBase.fHealthMax)
+		{
+			ICurrentBase.overrideSprite = sBlinkingRed;
+		}
 	}
 }
