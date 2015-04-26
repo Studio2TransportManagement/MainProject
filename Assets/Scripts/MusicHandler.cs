2433ebd8706e2b5ddb3eabbfef1ec02263aabb0d
David Hanna <onnfire@hotmail.com>
2015-04-26 16:50:41 +1000

    added script to GameManager to handle change of background music to death crys

---
+++ Assets/Scripts/MusicHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicHandler : MonoBehaviour {
+
+	public AudioSource asBackgroundMusic;
+	public AudioSource asDemotivationalPhrases;
+	public float fDimTimerInMinutes = 1;
+	
+	void Start ()
+	{
+		fDimTimerInMinutes = fDimTimerInMinutes * 60f;
+	}
+	
+	void Update ()
+	{
+		asBackgroundMusic.volume -= Time.deltaTime / fDimTimerInMinutes;
+		asDemotivationalPhrases.volume += Time.deltaTime / fDimTimerInMinutes;
+	}
+}
