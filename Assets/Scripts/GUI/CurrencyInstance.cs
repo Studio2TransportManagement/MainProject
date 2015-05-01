fa606b118da892573146d2fa5b05152f3c9f2ecf
David Hanna <onnfire@hotmail.com>
2015-05-01 19:18:07 +1000

    Added small feature to recruitment button to visually see when you acquire recruits and money. Added more UI images. Added workbench to bases to be used as method to open upgrade menus. Fixed trainline meshes from showing past stations.

---
+++ Assets/Scripts/GUI/CurrencyInstance.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class CurrencyInstance : MonoBehaviour {
+
+	public Text tThisText;
+
+	void Start ()
+	{
+		tThisText = gameObject.GetComponent<Text> ();
+		LeanTween.move (gameObject, gameObject.transform.position + new Vector3(0f, 20f, 0f), 2f).setEase (LeanTweenType.easeInQuad);
+	}
+
+	void Update () 
+	{
+		Color32 fadeColor = tThisText.color;
+		fadeColor.a -= 2;
+		tThisText.color = fadeColor;
+		
+		if(tThisText.color.a <= 0)
+		{
+			Destroy (gameObject);
+		}
+	}
+}
--- Assets/Scripts/PlayerResources.cs
+++ Assets/Scripts/PlayerResources.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class PlayerResources : MonoBehaviour {
@@ -6,6 +7,13 @@ public class PlayerResources : MonoBehaviour {
 	public float fStartingCash;
 	public float fStartingRecruits;
 
+	public Text tRecruitIncrease;
+	public RectTransform rectCurrencyPosition;
+	public Text tCurrency;
+	public Text tCurrencyInstance;
+	private Vector3 tranRecruitStartingPosition;
+	public Color32 colYellow;
+	public bool bIsRecruitFading;
 	public int iTotalRecruits;
 	public int iSelectedRecruits;
 	private float fCurrentMoney = 5000f;
@@ -16,16 +24,32 @@ public class PlayerResources : MonoBehaviour {
 	{
 		fCurrentMoney = fStartingCash;
 		fCurrentRecruits = fStartingRecruits;
+		tranRecruitStartingPosition = tRecruitIncrease.gameObject.transform.position;
+	}
+
+	void Update()
+	{
+		if(bIsRecruitFading)
+		{
+			Color32 fadeColor = tRecruitIncrease.color;
+			fadeColor.a -= 2;
+			tRecruitIncrease.color = fadeColor;
+			
+			if(tRecruitIncrease.color.a <= 0)
+			{
+				bIsRecruitFading = false;
+			}
+		}
 	}
 
 	//Setters
 	public void ChangeMoney(float amount) {
 		if (amount > 0) {
-			//add extra fluff here
+			IncreaseCurrency(amount);
 			fCurrentMoney += amount;
 		}
 		if (amount < 0) {
-			//fluff
+			IncreaseCurrency(amount);
 			fCurrentMoney += amount;
 		}
 	}
@@ -36,11 +60,11 @@ public class PlayerResources : MonoBehaviour {
 
 	public void ChangeRecruits(float amount) {
 		if (amount > 0) {
-			//add extra fluff here
+			IncreaseRecruits(amount);
 			fCurrentRecruits += amount;
 		}
 		if (amount < 0) {
-			//fluff
+			IncreaseRecruits(amount);
 			fCurrentRecruits += amount;
 		}
 	}
@@ -59,4 +83,22 @@ public class PlayerResources : MonoBehaviour {
 	public float GetRecruits(){
 		return fCurrentRecruits;
 	}
+
+	public void IncreaseRecruits(float amount) {
+		tRecruitIncrease.gameObject.transform.position = tranRecruitStartingPosition;
+		tRecruitIncrease.text = "+" + amount + " Recruit";
+		tRecruitIncrease.color = colYellow;
+		LeanTween.move (tRecruitIncrease.gameObject, tRecruitIncrease.gameObject.transform.position + new Vector3(0f, 20f, 0f), 2f).setEase (LeanTweenType.easeInQuad);
+		bIsRecruitFading = true;
+	}
+
+	public void IncreaseCurrency(float amount) {
+		tCurrencyInstance = Instantiate(tCurrency) as Text;
+		tCurrencyInstance.transform.SetParent(GameObject.Find("CurrencyPosition").transform, false);
+		tCurrencyInstance.transform.SetAsFirstSibling();
+		tCurrencyInstance.transform.position = rectCurrencyPosition.position;
+		tCurrencyInstance.text = "+" + amount + " Money";
+		tCurrencyInstance.color = colYellow;
+	}
+
 }
